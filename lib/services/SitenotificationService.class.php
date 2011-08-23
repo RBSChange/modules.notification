9eb2d03a81be45a9740b2c998c882888a4f64c4d
Franck STAUFFER <franck.stauffer@rbs.fr>
2011-08-23 11:32:09 +0200

    New class injection comptability

--- lib/services/SitenotificationService.class.php
+++ lib/services/SitenotificationService.class.php
@@ -17,7 +17,7 @@ class notification_SitenotificationService extends notification_NotificationServ
 	{
 		if (self::$instance === null)
 		{
-			self::$instance = self::getServiceClassInstance(get_class());
+			self::$instance = new self();
 		}
 		return self::$instance;
 	}
