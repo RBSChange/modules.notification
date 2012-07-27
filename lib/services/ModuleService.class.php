60d365cb40fa763d787bf03f4350e2854b585cfb
Eric HAUSWALD <eric.hauswald@rbs.fr>
2012-07-27 11:45:10 +0200

    [NEW #165] Permettre l'injection de LoggingService

--- lib/services/ModuleService.class.php
+++ lib/services/ModuleService.class.php
@@ -11,11 +11,6 @@ class notification_ModuleService extends ModuleBaseService
 	private static $instance = null;
 	
 	/**
-	 * @var string
-	 */
-	private $logFilePath;
-
-	/**
 	 * @return notification_ModuleService
 	 */
 	public static function getInstance()
@@ -27,24 +22,14 @@ class notification_ModuleService extends ModuleBaseService
 		return self::$instance;
 	}
 	
-	protected function __construct()
-	{
-		parent::__construct();
-		if (Framework::inDevelopmentMode() || Framework::isInfoEnabled())
-		{
-			$this->logFilePath = f_util_FileUtils::buildWebeditPath('log', 'notification', 'notification.log');
-			if (!file_exists($this->logFilePath))
-			{
-				f_util_FileUtils::writeAndCreateContainer($this->logFilePath, gmdate('Y-m-d H:i:s')."\t Created" . PHP_EOL);
-			}
-		}
-	}
-	
+	/**
+	 * @param string $stringLine
+	 */
 	public function log($stringLine)
 	{
-		if ($this->logFilePath !== null)
+		if (Framework::inDevelopmentMode() || Framework::isInfoEnabled())
 		{
-			error_log(gmdate('Y-m-d H:i:s')."\t".$stringLine . PHP_EOL, 3, $this->logFilePath);
+			LoggingService::getInstance()->namedLog($stringLine, 'notification');
 		}
 	}
 }
