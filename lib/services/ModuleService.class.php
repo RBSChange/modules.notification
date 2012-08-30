f87b3e0c36e54732b252e19caf3d4bd0bf427bd2
Gaël PORT <gael.port@rbs.fr>
2012-08-30 15:45:04 +0200

    [NEW] Merge with 3.6

--- lib/services/ModuleService.class.php
+++ lib/services/ModuleService.class.php
@@ -6,31 +6,13 @@
 class notification_ModuleService extends ModuleBaseService
 {	
 	/**
-	 * @var string
-	 */
-	private $logFilePath;
-	
-	protected function __construct()
-	{
-		parent::__construct();
-		if (Framework::inDevelopmentMode() || Framework::isInfoEnabled())
-		{
-			$this->logFilePath = f_util_FileUtils::buildProjectPath('log', 'notification', 'notification.log');
-			if (!file_exists($this->logFilePath))
-			{
-				f_util_FileUtils::writeAndCreateContainer($this->logFilePath, gmdate('Y-m-d H:i:s') . "\t Created" . PHP_EOL);
-			}
-		}
-	}
-	
-	/**
 	 * @param string $stringLine
 	 */
 	public function log($stringLine)
 	{
-		if ($this->logFilePath !== null)
+		if (Framework::inDevelopmentMode() || Framework::isInfoEnabled())
 		{
-			error_log(gmdate('Y-m-d H:i:s') . "\t" . $stringLine . PHP_EOL, 3, $this->logFilePath);
+			change_LoggingService::getInstance()->namedLog($stringLine, 'notification');
 		}
 	}
 }
