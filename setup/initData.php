d317f416262cd1857da34fa219c01118608ec611
Gaël PORT <gael.port@rbs.fr>
2012-09-19 17:45:11 +0200

    [DELETE] Delete old samples.

--- setup/initData.php
+++ setup/initData.php
@@ -3,19 +3,10 @@ class notification_Setup extends object_InitDataSetup
 {
 	public function install()
 	{
-		try
-		{
-			$scriptReader = import_ScriptReader::getInstance();
-	   	 	$scriptReader->executeModuleScript('notification', 'init.xml');
-	   	 	if (ModuleService::getInstance()->moduleExists('webservices'))
-	   	 	{
-	   	 		$scriptReader->executeModuleScript('notification', 'webservices.xml');
-	   	 	}
-		}
-		catch (Exception $e)
-		{
-			echo "ERROR: " . $e->getMessage() . "\n";
-			Framework::exception($e);
-		}
+		$this->executeModuleScript('init.xml');
+   	 	if (ModuleService::getInstance()->moduleExists('webservices'))
+   	 	{
+   	 		$this->executeModuleScript('webservices.xml');
+   	 	}
 	}
 }
