6f5c3cb32931454bae38053c40369da9b86477d2
Gaël PORT <gael.port@rbs.fr>
2013-01-14 16:01:12 +0100

    [NEW #44] templates de notifications : union des templates présents dans override et dans le module

--- lib/services/ListTemplatesService.class.php
+++ lib/services/ListTemplatesService.class.php
@@ -9,7 +9,7 @@ class notification_ListTemplatesService extends BaseService implements list_List
 	 * @var notification_ListTemplatesService
 	 */
 	private static $instance;
-
+	
 	/**
 	 * @return notification_ListTemplatesService
 	 */
@@ -21,44 +21,44 @@ class notification_ListTemplatesService extends BaseService implements list_List
 		}
 		return self::$instance;
 	}
-
-
+	
 	/**
 	 * Returns an array of available templates for the notification module.
-	 *
 	 * @return array
 	 */
 	public function getItems()
 	{
-		$dir = new DirectoryIterator(FileResolver::getInstance()->setPackageName('modules_notification')->getPath('templates'));
-
 		$templateArray = array();
-        foreach ($dir as $file)
-        {
-            if ($file->isReadable() && $file->isFile())
-            {
-	        	$fileName = $file->getFileName();
-            	if (f_util_StringUtils::endsWith($fileName, '.all.all.html'))
-	        	{
-	        		$template = substr($fileName, 0, -13);
-	        	}
-	        	else if (f_util_StringUtils::endsWith($fileName, '.all.all.txt'))
-	        	{
-	        		$template = substr($fileName, 0, -12);
-	        	}
-        		if (!in_array($template, $templateArray))
-        		{
-        			$templateArray[] = $template;
-        		}
-            }
-        }
-
-        $itemArray = array();
-        foreach ($templateArray as $template)
-        {
-            	$itemArray[] = new list_Item($template, $template);
-        }
-
+		foreach (FileResolver::getInstance()->setPackageName('modules_notification')->getPaths('templates') as $path)
+		{
+			$dir = new DirectoryIterator($path);
+			foreach ($dir as $file)
+			{
+				if ($file->isReadable() && $file->isFile())
+				{
+					$fileName = $file->getFileName();
+					if (f_util_StringUtils::endsWith($fileName, '.all.all.html'))
+					{
+						$template = substr($fileName, 0, -13);
+					}
+					elseif (f_util_StringUtils::endsWith($fileName, '.all.all.txt'))
+					{
+						$template = substr($fileName, 0, -12);
+					}
+					if (!in_array($template, $templateArray))
+					{
+						$templateArray[] = $template;
+					}
+				}
+			}
+		}
+		
+		$itemArray = array();
+		foreach ($templateArray as $template)
+		{
+			$itemArray[] = new list_Item($template, $template);
+		}
+		
 		return $itemArray;
 	}
 }
